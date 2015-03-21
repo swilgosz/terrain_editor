1a9d1f45683bc80e6cea4e7cf9b689ce527981f2
Sebastian Wilgosz <sebastian.wilgosz@useo.pl>
2015-03-21 20:59:12 +0100

    Static pages + bootstrap integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'welcome/index'
+  get 'welcome/about', as: "about"
+  get 'welcome/contact', as: "contact"
+
   devise_for :users, path: "/", path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'users#index'
+  root 'welcome#index'
   # You can have the root of your site routed with "root"
 
   # Example of regular route:
